c462ac6fdef39728061b176a6a920f0b3e98d1f9
Wishmastering <charsdb@gmail.com>
2023-10-03 16:51:43 -0600

    oAUTH 2.0 & PKCE study and investigate

--- calc.spec.js
+++ calc.spec.js
@@ -4,8 +4,11 @@ it("should add two numbers", () => {
   //Assert
 });
 
-it.todo("should multiply two number", () => {
-  // Arrange
-  //Act
-  //Assert
-});
+it.todo(
+  "should multiply two number // oAUTH 2.0 & PKCE study and investigate",
+  () => {
+    // Arrange
+    // Act
+    // Assert
+  }
+);
